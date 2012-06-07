851e6a99ae81dbf42b3fa2dfa90c8d539d385eb9
Chris Roberts <chrisroberts.code@gmail.com>
2012-06-07 12:21:56 -0700

    Be lax about required attributes. Allow chef to be opt-in

--- resources/container.rb
+++ resources/container.rb
@@ -6,9 +6,10 @@ end
 actions :create, :delete, :clone
 
 attribute :base_container, :kind_of => String
-attribute :validation_client, :kind_of => String, :required => true
+attribute :validation_client, :kind_of => String
 attribute :validator_pem, :kind_of => String
-attribute :server_uri, :kind_of => String, :required => true
+attribute :server_uri, :kind_of => String
 attribute :node_name, :kind_of => String
-attribute :run_list, :kind_of => Array, :required => true
-
+attribute :run_list, :kind_of => Array
+attribute :chef_enabled, :kind_of => [TrueClass, FalseClass], :default => true
+attribute :static_ip, :kind_of => String, :default => false
