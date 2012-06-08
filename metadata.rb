801445b22028619e4e70a359e1a5812a1905b88f
Chris Roberts <chrisroberts.code@gmail.com>
2012-06-08 14:44:39 -0700

    Add omnibus_updater dependency. Add hard version requirement for ubuntu

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,6 @@ description      "Chef driven Linux Containers"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
+supports 'ubuntu', '12.04'
+
+depends 'omnibus_updater'
