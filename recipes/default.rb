6db2a6813ea7c0d57cfc1ab0764d714e278401bd
Chris Roberts <chrisroberts.code@gmail.com>
2012-06-08 14:44:19 -0700

    Ensure omnibus package is available

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,3 +4,6 @@ cookbook_file "/usr/lib/lxc/templates/lxc-ubuntu-hw" do
   source 'lxc-ubuntu-hw'
   mode 0755
 end
+
+node.set[:omnibus_updater][:cache_omnibus_installer] = true
+include_recipe 'omnibus_updater::deb_downloader'
