1a7ac426bca661fb1c0e69f370de460ce7141085
Chris Roberts <chrisroberts.code@gmail.com>
2012-06-27 09:28:12 -0700

    Allow separate sets of commands for new vs. updated containers. Add new container attribute

--- resources/container.rb
+++ resources/container.rb
@@ -7,7 +7,7 @@ actions :create, :delete, :clone
 
 attribute :base_container, :kind_of => String
 attribute :validation_client, :kind_of => String
-attribute :validator_pem, :kind_of => String
+attribute :validator_pem, :kind_of => String, :default => nil
 attribute :server_uri, :kind_of => String
 attribute :node_name, :kind_of => String
 attribute :run_list, :kind_of => Array
@@ -15,3 +15,5 @@ attribute :chef_enabled, :kind_of => [TrueClass, FalseClass], :default => true
 attribute :static_ip, :kind_of => String, :default => false
 attribute :config, :kind_of => Hash
 attribute :container_commands, :kind_of => Array, :default => []
+attribute :initialize_commands, :kind_of => Array, :default => []
+attribute :new_container, :kind_of => [TrueClass, FalseClass], :default => false
