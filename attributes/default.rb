448d0617d171b375f68e0ba35698ed0c0548ac13
Chris Roberts <chrisroberts.code@gmail.com>
2012-07-06 11:55:49 -0700

    Provide default lxc configuration

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,11 @@
 default[:lxc][:start_ipaddress] = nil
 default[:lxc][:validator_pem] = nil
+default[:lxc][:auto_start] = true
+default[:lxc][:use_bridge] = true
+default[:lxc][:bridge] = 'lxcbr0'
+default[:lxc][:addr] = '10.0.3.1'
+default[:lxc][:netmask] = '255.255.255.0'
+default[:lxc][:network] = '10.0.3.0/24'
+default[:lxc][:dhcp_range] = '10.0.3.2,10.0.3.254'
+default[:lxc][:dhcp_max] = '253'
+default[:lxc][:shutdown_timeout] = 120
--- recipes/default.rb
+++ recipes/default.rb
@@ -5,5 +5,24 @@ cookbook_file "/usr/lib/lxc/templates/lxc-ubuntu-hw" do
   mode 0755
 end
 
+template '/etc/default/lxc' do
+  source 'default-lxc.erb'
+  mode 0644
+  variables(
+    :config => {
+      :lxc_auto => node[:lxc][:auto_start],
+      :use_lxc_bridge => node[:lxc][:use_bridge],
+      :lxc_bridge => node[:lxc][:bridge],
+      :lxc_addr => node[:lxc][:addr],
+      :lxc_netmask => node[:lxc][:netmask],
+      :lxc_network => node[:lxc][:network],
+      :lxc_dhcp_range => node[:lxc][:dhcp_range],
+      :lxc_dhcp_max => node[:lxc][:dhcp_max],
+      :lxc_shutdown_timeout => node[:lxc][:shutdown_timeout]
+    }
+  )
+  # notify?
+end
+
 node.set[:omnibus_updater][:cache_omnibus_installer] = true
 include_recipe 'omnibus_updater::deb_downloader'
