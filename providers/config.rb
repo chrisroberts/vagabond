bc64250e243d9386e5f797d4f0fd07d5cc54ac7d
Chris Roberts <chrisroberts.code@gmail.com>
2012-09-10 21:24:29 -0700

    Provide more robust configuration vi LWRP

--- providers/config.rb
+++ providers/config.rb
@@ -1,62 +1,46 @@
 require 'securerandom'
 
 def load_current_resource
-  if(::File.exists?(Lxc.container_config(new_resource.name)))
-    mac_addr = ::File.readlines(Lxc.container_config(new_resource.name)).detect{|line|
-      line.include?('hwaddr')
-    }.to_s.split('=').last.to_s.strip
+  new_resource.utsname new_resource.name unless new_resource.utsname
+  new_resource.rootfs "/var/lib/lxc/#{new_resource.utsname}/rootfs" unless new_resource.rootfs
+  new_resource.mount "/var/lib/lxc/#{new_resource.utsname}/fstab" unless new_resource.mount
+  config = LxcFileConfig.new(Lxc.container_config(new_resource.utsname))
+  if((new_resource.network.nil? || new_resource.network.empty?))
+    if(config.network.empty?)
+      new_resource.network(
+        :type => :veth,
+        :link => node[:lxc][:bridge],
+        :flags => :up,
+        :hwaddr => "00:16:3e#{SecureRandom.hex(3).gsub(/(..)/, ':\1')}"
+      )
+    else
+      new_resource.network(config.network.first)
+    end
+  else
+    [new_resource.network].flatten.each_with_index do |net_hash, idx|
+      if(config.network[idx].nil? || config.network[idx][:hwaddr].nil?)
+        net_hash[:hwaddr] ||= "00:16:3e#{SecureRandom.hex(3).gsub(/(..)/, ':\1')}"
+      end
+    end
   end
-  if(mac_addr.to_s.empty?)
-    mac_addr = "00:16:3e#{SecureRandom.hex(3).gsub(/(..)/, ':\1')}" 
-  end
-  base_config = {
-    'lxc.network.type' => 'veth',
-    'lxc.network.link' => 'lxcbr0',
-    'lxc.network.flags' => 'up',
-    'lxc.network.hwaddr' => mac_addr,
-    'lxc.utsname' => new_resource.name,
-    'lxc.devttydir' => 'lxc',
-    'lxc.tty' => 4,
-    'lxc.pts' => 1024,
-    'lxc.arch' => 'amd64',
-    'lxc.rootfs' => "/var/lib/lxc/#{new_resource.name}/rootfs",
-    'lxc.mount'  => "/var/lib/lxc/#{new_resource.name}/fstab",
-    'lxc.cap.drop' => 'sys_module mac_admin',
-    'lxc.cgroup.devices.deny' => 'a',
-    'lxc.cgroup.devices.allow' => [
-      'c *:* m',
-      'b *:* m',
-      'c 1:3 rwm',
-      'c 1:5 rwm',
-      'c 5:1 rwm',
-      'c 5:0 rwm',
-      'c 1:9 rwm',
-      'c 1:8 rwm',
-      'c 136:* rwm',
-      'c 5:2 rwm',
-      'c 254:0 rwm',
-      'c 10:229 rwm',
-      'c 10:200 rwm',
-      'c 1:7 rwm',
-      'c 10:228 rwm',
-      'c 10:232 rwm'
-    ]
-  }
-  new_resource.config Chef::Mixin::DeepMerge.merge(base_config, new_resource.config)
 end
 
 action :create do
-  ruby_block "lxc config_updater[#{new_resource.name}]" do
+  ruby_block "lxc config_updater[#{new_resource.utsname}]" do
     block do
       new_resource.updated_by_last_action(true)
     end
     action :nothing
   end
 
-  file "lxc update_config[#{new_resource.name}]" do
-    path Lxc.container_config(new_resource.name)
-    content Lxc.generate_config(new_resource.name, new_resource.config)
+  directory Lxc.container_path(new_resource.utsname) do
+    action :create
+  end
+
+  file "lxc update_config[#{new_resource.utsname}]" do
+    path Lxc.container_config(new_resource.utsname)
+    content LxcFileConfig.generate_config(new_resource)
     mode 0644
-    notifies :create, resources(:ruby_block => "lxc config_updater[#{new_resource.name}]"), :immediately
+    notifies :create, resources(:ruby_block => "lxc config_updater[#{new_resource.utsname}]"), :immediately
   end
 end
