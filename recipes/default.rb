bd452d0f0fb3989fe5e95881009a292eb63d7f2a
Chris Roberts <chrisroberts.code@gmail.com>
2012-11-08 08:40:44 -0800

    Remove deprecated template file

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,11 +2,6 @@ package 'lxc'
 
 include_recipe 'lxc::install_dependencies'
 
-cookbook_file "/usr/lib/lxc/templates/lxc-ubuntu-hw" do
-  source 'lxc-ubuntu-hw'
-  mode 0755
-end
-
 template '/etc/default/lxc' do
   source 'default-lxc.erb'
   mode 0644
