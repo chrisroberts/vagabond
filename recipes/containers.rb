aff1d58235a5079aec31dd0db990fa7fae155911
Matt Ray <matthewhray@gmail.com>
2012-11-20 17:23:16 -0600

    Added the 'containers' recipe to create containers for the members of the node['lxc']['containers'] hash

---
+++ recipes/containers.rb
@@ -0,0 +1,14 @@
+# create the containers defined in the ['lxc']['containers'] hash
+
+include_recipe "lxc"
+
+node['lxc']['containers'].each do | name, container |
+  Chef::Log.info "Creating LXC container name:#{name}"
+  Chef::Log.info "Creating LXC container container:#{container}"
+  lxc_container name do
+    action :create
+    template container['template'] if container['template']
+    # this is getting refactored out into another recipe eventually
+    chef_enabled false
+  end
+end
