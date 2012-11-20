83dd507ed35dbe9a26587042cfe21835fc0e24aa
Chris Roberts <chrisroberts.code@gmail.com>
2012-11-20 10:16:45 -0800

    Refactor lxc_service LWRP

--- providers/service.rb
+++ providers/service.rb
@@ -10,71 +10,44 @@ def load_current_resource
 end
 
 action :start do
-
-  ruby_block "lxc start[#{new_resource.service_name}]" do
-    block do
-      new_resource._lxc.start
-    end
-    only_if do
-      !new_resource._lxc.running? && new_resource.updated_by_last_action(true)
-    end
+  if(new_resource._lxc.stopped?)
+    new_resource._lxc.start
+    new_resource.updated_by_last_action(true)
   end
-
 end
 
 action :halt do
-  ruby_block "lxc halt[#{new_resource.service_name}]" do
-    block do
-      new_resource._lxc.stop
-    end
-    only_if do
-      new_resource._lxc.running? && new_resource.updated_by_last_action(true)
-    end
+  if(new_resource._lxc.running?)
+    new_resource._lxc.stop
+    new_resource.updated_by_last_action(true)
   end
 end
 
-# TODO: Should we wait for stop and then wait for start here?
 action :restart do
-  ruby_block "lxc restart[#{new_resource.service_name}]" do
-    block do
-      new_resource._lxc.shutdown
-      new_resource._lxc.start
-    end
-    only_if do
-      new_resource._lxc.running? && new_resource.updated_by_last_action(true)
-    end
+  if(new_resource._lxc.running?)
+    new_resource._lxc.shutdown
   end
+  new_resource._lxc.start
+  new_resource.updated_by_last_action(true)
 end
 
 action :stop do
-  ruby_block "lxc stop[#{new_resource.service_name}]" do
-    block do
-      new_resource._lxc.shutdown
-    end
-    only_if do
-      new_resource._lxc.running? && new_resource.updated_by_last_action(true)
-    end
+  if(new_resource._lxc.running?)
+    new_resource._lxc.stop
+    new_resource.updated_by_last_action(true)
   end
 end
 
 action :freeze do
-  ruby_block "lxc freeze[#{new_resource.service_name}]" do
-    ruby_block do
-      new_resource._lxc.freeze
-    end
-    only_if do
-      new_resource._lxc.running? && new_resource.updated_by_last_action(true)
-    end
+  if(new_resource._lxc.running?)
+    new_resource._lxc.freeze
+    new_resource.updated_by_last_action(true)
   end
 end
 
 action :unfreeze do
-  ruby_block "lxc unfreeze[#{new_resource.service_name}]" do
-    block do
-      new_resource._lxc.unfreeze
-    end
-    only_if do
-      new_resource._lxc.frozen? && new_resource.updated_by_last_action(true)
-    end
+  if(new_resource._lxc.frozen?)
+    new_resource._lxc.unfreeze
+    new_resource.updated_by_last_action(true)
   end
 end
