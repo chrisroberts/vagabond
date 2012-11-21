2ca01767466a901fb72c2ff8390a05a424a90c7e
Matt Ray <matthewhray@gmail.com>
2012-11-21 15:58:54 -0600

    use the methods and parameters straight from the hash

--- recipes/containers.rb
+++ recipes/containers.rb
@@ -5,8 +5,10 @@ include_recipe "lxc"
 node['lxc']['containers'].each do | name, container |
   Chef::Log.info "Creating LXC container name:#{name}"
   lxc_container name do
-    action :create
-    template container['template'] if container['template']
+    container.each do |meth, param|
+      self.send(meth, param)
+    end
+    action :create unless container.has_key?(:action)
     # this is getting refactored out into another recipe eventually
     chef_enabled false
   end
