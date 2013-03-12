8219b12e645bf38ca25840366677ac11e6e6f2b7
Chris Roberts <chrisroberts.code@gmail.com>
2013-03-11 18:21:28 -0700

    Update centos_63 base attributes

--- lib/vagabond/cookbooks/vagabond/attributes/default.rb
+++ lib/vagabond/cookbooks/vagabond/attributes/default.rb
@@ -1,3 +1,4 @@
 default[:vagabond][:bases][:ubuntu_1204][:template_options] = {'--release' => 'precise'}
 default[:vagabond][:bases][:ubuntu_1210][:template_options] = {'--release' => 'quantal'}
-default[:vagabond][:bases][:centos_63][:template_options] => {'--release' => '6.3'}
+default[:vagabond][:bases][:centos_63][:template] = 'centos'
+default[:vagabond][:bases][:centos_63][:template_options] = {'--release' => '6', '--releaseminor' => '3'}
