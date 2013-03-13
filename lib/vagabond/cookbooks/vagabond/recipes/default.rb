5ec91c09c66d1d1f7e4e5c4965772ab1afecb92e
Chris Roberts <chrisroberts.code@gmail.com>
2013-03-13 08:18:49 -0700

    Add more base templates and set to only create if enabled

--- lib/vagabond/cookbooks/vagabond/recipes/default.rb
+++ lib/vagabond/cookbooks/vagabond/recipes/default.rb
@@ -6,17 +6,38 @@ cookbook_file '/usr/share/lxc/templates/lxc-centos' do
 end
 
 node[:vagabond][:bases].each do |name, options|
+  
+  next unless options[:enabled]
+
+  pkg_coms = [
+    'update -y -q',
+    'upgrade -y -q',
+    'install curl -y -q'
+  ]
+  if(%w(debian ubuntu).include?(options[:template]))
+    pkg_man = 'apt-get'
+  elsif(%w(fedora centos).include?(options[:template]))
+    pkg_man = 'yum'
+  end
+  if(pkg_man)
+    pkg_coms.map! do |c|
+      "#{pkg_man} #{c}"
+    end
+  else
+    pkg_coms = []
+  end
 
   lxc_container name do
     template options[:template]
     template_opts options[:template_options]
     default_config false if options[:memory]
+    create_environment options[:environment] if options[:environment]
     initialize_commands [
       'rm -f /etc/sysctl.d/10-console-messages.conf',
       'rm -f /etc/sysctl.d/10-ptrace.conf',
-      'rm -f /etc/sysctl.d/10-kernel-hardening.conf',
-      "#{options[:template] == 'centos' ? 'yum install -q -y curl' : 'apt-get install -q -y curl'}",
-      'curl -L https://www.opscode.com/chef/install.sh | sudo bash'
+      'rm -f /etc/sysctl.d/10-kernel-hardening.conf'
+    ] + pkg_coms + [
+      'curl -L https://www.opscode.com/chef/install.sh | bash'
     ]
   end
 
