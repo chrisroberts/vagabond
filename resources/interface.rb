3fc6308c77f875a2de74fd9a4bbe60b83eb18bcd
Chris Roberts <chrisroberts.code@gmail.com>
2013-03-29 09:00:06 -0700

    Don't force regex match on addressing

--- resources/interface.rb
+++ resources/interface.rb
@@ -5,7 +5,7 @@ attribute :container, :kind_of => String, :required => true
 attribute :device, :kind_of => String, :required => true
 attribute :auto, :kind_of => [TrueClass, FalseClass], :default => true
 attribute :dynamic, :kind_of => [TrueClass, FalseClass], :default => false
-attribute :address, :regex => %r{\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}}
-attribute :gateway, :regex => %r{\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}}
-attribute :netmask, :regex => %r{\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}}, :default => '255.255.255.0'
+attribute :address, :kind_of => String
+attribute :gateway, :kind_of => String
+attribute :netmask, :kind_of => [String,Numeric]
 attribute :ipv6, :kind_of => [TrueClass,FalseClass], :default => false
