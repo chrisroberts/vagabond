93d94dc570f4174f5f721ff5b2a53c07147c1fa4
Chris Roberts <chrisroberts.code@gmail.com>
2013-03-30 12:16:46 -0700

    Add stream output attribute to ephemerals

--- resources/ephemeral.rb
+++ resources/ephemeral.rb
@@ -8,3 +8,4 @@ attribute :background, :kind_of => [TrueClass,FalseClass], :default => false
 attribute :union_type, :equal_to => %w(aufs overlayfs), :default => 'overlayfs'
 attribute :user, :kind_of => String, :default => 'root'
 attribute :key, :kind_of => String, :default => '/opt/hw-lxc-config/id_rsa'
+attribute :stream_output, :kind_of => [TrueClass,FalseClass,IO]
