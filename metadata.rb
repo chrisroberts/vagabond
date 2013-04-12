e8caac95a26ebb0ced11677480ce446df0e446f7
Chris Roberts <chrisroberts.code@gmail.com>
2013-04-12 14:40:17 -0700

    Update version for development 1.0.1

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email 'chrisroberts.code@gmail.com'
 license          'Apache 2.0'
 description      'Chef driven Linux Containers'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version          '1.0.1'
 
 supports 'ubuntu'
 
