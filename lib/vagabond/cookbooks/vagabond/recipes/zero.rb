6824c499f084e0f4030013b717edf463bc29c705
Chris Roberts <chrisroberts.code@gmail.com>
2013-05-06 12:36:29 -0700

    Scrub the vagabond cookbook

--- lib/vagabond/cookbooks/vagabond/recipes/zero.rb
+++
@@ -1,6 +0,0 @@
-gem_package 'chef-zero'
-
-execute 'start chef-zero' do
-  command "#{File.join(node[:ruby][:gem][:bin_dir]} chef-zero start -h #{node[:ipaddress]} -p 80 &"
-  not_if 'netstat -lpt | grep "tcp[[:space:]]" | grep ruby'
-end
