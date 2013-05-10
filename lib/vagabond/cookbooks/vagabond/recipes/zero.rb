8a14f0058ef9f4dd4ce285f17621d5e96d19d131
Chris Roberts <chrisroberts.code@gmail.com>
2013-05-10 11:47:41 -0700

    Disown the process after it's started

--- lib/vagabond/cookbooks/vagabond/recipes/zero.rb
+++ lib/vagabond/cookbooks/vagabond/recipes/zero.rb
@@ -5,4 +5,10 @@ gem_package 'chef-zero'
 execute 'start chef-zero' do
   command "#{File.join(node[:languages][:ruby][:bin_dir],'chef-zero')} start -H #{node[:ipaddress]} -p 80 &"
   not_if 'netstat -lpt | grep "tcp[[:space:]]" | grep ruby'
+  notifies :run, 'bash[disown-chef]', :immediately
+end
+
+bash 'disown chef-zero' do
+  action :nothing
+  command 'disown %1'
 end
