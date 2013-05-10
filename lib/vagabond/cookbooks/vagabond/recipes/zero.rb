128c6684ae658900e3971815cd90f96580bd8c6a
Chris Roberts <chrisroberts.code@gmail.com>
2013-05-10 12:00:31 -0700

    Just use the platform helper and do it right

--- lib/vagabond/cookbooks/vagabond/recipes/zero.rb
+++ lib/vagabond/cookbooks/vagabond/recipes/zero.rb
@@ -3,12 +3,7 @@ package 'build-essential'
 gem_package 'chef-zero'
 
 execute 'start chef-zero' do
-  command "#{File.join(node[:languages][:ruby][:bin_dir],'chef-zero')} start -H #{node[:ipaddress]} -p 80 &"
+  command "start-stop-daemon --background --start --quiet --exec #{File.join(node[:languages][:ruby][:bin_dir],'chef-zero')} -- -H #{node[:ipaddress]} -p 80 start"
   not_if 'netstat -lpt | grep "tcp[[:space:]]" | grep ruby'
-  notifies :run, 'bash[disown chef-zero]', :immediately
 end
 
-bash 'disown chef-zero' do
-  action :nothing
-  command 'disown %1'
-end
