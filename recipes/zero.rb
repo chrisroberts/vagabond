e0775f2a73de20701012a9f6a07b2bc14000000e
Chris Roberts <chrisroberts.code@gmail.com>
2013-05-10 11:27:04 -0700

    Fix some mucked up syntax

--- recipes/zero.rb
+++ recipes/zero.rb
@@ -3,6 +3,6 @@ package 'build-essential'
 gem_package 'chef-zero'
 
 execute 'start chef-zero' do
-  command "#{File.join(node[:languages][:ruby]:bin_dir]} chef-zero start -H #{node[:ipaddress]} -p 80 &"
+  command "#{File.join(node[:languages][:ruby][:bin_dir],'chef-zero')} start -H #{node[:ipaddress]} -p 80 &"
   not_if 'netstat -lpt | grep "tcp[[:space:]]" | grep ruby'
 end
