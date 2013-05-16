b0092e7027e33aaf39f155659141de8d574139de
Chris Roberts <chrisroberts.code@gmail.com>
2013-05-16 09:14:33 -0700

    Provide helper module for commons. Add basic polling.

--- libraries/lxc.rb
+++ libraries/lxc.rb
@@ -5,6 +5,67 @@ class Lxc
   class CommandFailed < StandardError
   end
 
+  module Helpers
+    
+    # Simple helper to shell out
+    def run_command(cmd, args={})
+      retries = args[:allow_failure_retry].to_i
+      begin
+        shlout = Mixlib::ShellOut.new(cmd, 
+          :logger => defined?(Chef) ? Chef::Log.logger : log,
+          :live_stream => args[:livestream] ? STDOUT : nil,
+          :timeout => args[:timeout] || 1200,
+          :environment => {'HOME' => detect_home}
+        )
+        shlout.run_command
+        shlout.error!
+        shlout
+      rescue Mixlib::ShellOut::ShellCommandFailed, CommandFailed, Mixlib::ShellOut::CommandTimeout
+        if(retries > 0)
+          log.warn "LXC run command failed: #{cmd}"
+          log.warn "Retrying command. #{args[:allow_failure_retry].to_i - retries} of #{args[:allow_failure_retry].to_i} retries remain"
+          sleep(0.3)
+          retries -= 1
+          retry
+        elsif(args[:allow_failure])
+          true
+        else
+          raise
+        end
+      end
+    end
+
+    def command(*args)
+      run_command(*args)
+    end
+    
+    def log
+      if(defined?(Chef))
+        Chef::Log
+      else
+        unless(@logger)
+          require 'logger'
+          @logger = Logger.new('/dev/null')
+        end
+        @logger
+      end
+    end
+    
+    # Detect HOME environment variable. If not an acceptable
+    # value, set to /root or /tmp
+    def detect_home(set_if_missing=false)
+      if(ENV['HOME'] && Pathname.new(ENV['HOME']).absolute?)
+        ENV['HOME']
+      else
+        home = File.directory?('/root') && File.writable?('/root') ? '/root' : '/tmp'
+        if(set_if_missing)
+          ENV['HOME'] = home
+        end
+        home
+      end
+    end
+  end
+
   # Pathname#join does not act like File#join when joining paths that
   # begin with '/', and that's dumb. So we'll make our own Pathname,
   # with a #join that uses File
@@ -14,10 +75,14 @@ class Lxc
     end
   end
   
+  include Helpers
+
   attr_reader :name
 
   class << self
 
+    include Helpers
+    
     attr_accessor :use_sudo
 
     def sudo
@@ -54,17 +119,23 @@ class Lxc
     def list
       %x{#{sudo}lxc-ls}.split("\n").uniq
     end
-
+  
+    
     # name:: Name of container
     # Returns information about given container
     def info(name)
       res = {:state => nil, :pid => nil}
-      info = %x{#{sudo}lxc-info -n #{name}}.split("\n")
-      parts = info.first.split(' ')
-      res[:state] = parts.last.downcase.to_sym
-      parts = info.last.split(' ')
-      res[:pid] = parts.last.to_i
-      res
+      info = run_command("#{sudo}lxc-info -n #{name}").stdout.split("\n")
+      if(info.first)
+        parts = info.first.split(' ')
+        res[:state] = parts.last.downcase.to_sym
+        parts = info.last.split(' ')
+        res[:pid] = parts.last.to_i
+        res
+      else
+        res[:state] = :unknown
+        res[:pid] = -1
+      end
     end
 
     # Return full container information list
@@ -231,37 +302,41 @@ class Lxc
   end
 
   # Start the container
-  def start
-    run_command("#{sudo}lxc-start -n #{name} -d")
-    run_command("#{sudo}lxc-wait -n #{name} -s RUNNING", :allow_failure_retry => 2)
+  def start(*args)
+    if(args.include?(:no_daemon))
+      run_command("#{sudo}lxc-start -n #{name}")
+    else
+      run_command("#{sudo}lxc-start -n #{name} -d")
+      wait_for_state(:running)
+    end
   end
 
   # Stop the container
   def stop
     run_command("#{sudo}lxc-stop -n #{name}", :allow_failure_retry => 3)
-    run_command("#{sudo}lxc-wait -n #{name} -s STOPPED", :allow_failure_retry => 2)
+    wait_for_state(:stopped)
   end
   
   # Freeze the container
   def freeze
     run_command("#{sudo}lxc-freeze -n #{name}")
-    run_command("#{sudo}lxc-wait -n #{name} -s FROZEN", :allow_failure_retry => 2)
+    wait_for_state(:frozen)
   end
 
   # Unfreeze the container
   def unfreeze
     run_command("#{sudo}lxc-unfreeze -n #{name}")
-    run_command("#{sudo}lxc-wait -n #{name} -s RUNNING", :allow_failure_retry => 2)
+    wait_for_state(:running)
   end
 
   # Shutdown the container
   def shutdown
     run_command("#{sudo}lxc-shutdown -n #{name}")
-    run_command("#{sudo}lxc-wait -n #{name} -s STOPPED", :allow_failure => true, :timeout => 120)
+    wait_for_state(:stopped, :timeout => 120)
     # This block is for fedora/centos/anyone else that does not like lxc-shutdown
     if(running?)
       container_command('shutdown -h now')
-      run_command("#{sudo}lxc-wait -n #{name} -s STOPPED", :allow_failure => true)
+      wait_for_state(:stopped, :timeout => 120)
       # If still running here, something is wrong
       if(running?)
         raise "Failed to shutdown container: #{name}"
@@ -292,12 +367,13 @@ class Lxc
     begin
       shlout = Mixlib::ShellOut.new(cmd, 
         :logger => defined?(Chef) ? Chef::Log.logger : log,
-        :live_stream => STDOUT,
+        :live_stream => args[:livestream] ? nil : STDOUT,
         :timeout => args[:timeout] || 1200,
         :environment => {'HOME' => detect_home}
       )
       shlout.run_command
       shlout.error!
+      shlout
     rescue Mixlib::ShellOut::ShellCommandFailed, CommandFailed, Mixlib::ShellOut::CommandTimeout
       if(retries > 0)
         log.warn "LXC run command failed: #{cmd}"
@@ -310,6 +386,15 @@ class Lxc
       else
         raise
       end
+    end
+  end
+
+  def wait_for_state(desired_state, args={})
+    args[:sleep_interval] ||= 0.4
+    wait_total = 0.0
+    until(state == desired_state.to_sym || (args[:timeout].to_i > 0 && wait_total.to_i > args[:timeout].to_i))
+      sleep(args[:sleep_interval])
+      wait_total += args[:sleep_interval]
     end
   end
 
