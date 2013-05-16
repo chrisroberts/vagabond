3a7d55fb16271ce109e5a71a0e0190a8f920a9b5
Chris Roberts <chrisroberts.code@gmail.com>
2013-05-16 09:06:06 -0700

    Fix bad syntax

--- lib/vagabond/monkey/kitchen_config.rb
+++ lib/vagabond/monkey/kitchen_config.rb
@@ -4,13 +4,14 @@ module Vagabond
   module MonkeyPatch
     module KitchenConfig
       def clusters
-        @clusters Mash.new[
+        @clusters = Mash[
           *(
             Array(data[:clusters]).map{ |name|
               [name, suites.detect{ |suite| suite.name == name }]
             }.flatten
           )
         ]
+      end
     end
   end
 end
