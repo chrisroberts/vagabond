9c49d7e94aec0d2c583f511f3b5a6e350b5d50c5
Chris Roberts <chrisroberts.code@gmail.com>
2013-06-17 08:55:00 -0700

    Add `#exit_code`. Include new error for failed spec run.

--- lib/vagabond/errors.rb
+++ lib/vagabond/errors.rb
@@ -3,6 +3,9 @@ module Vagabond
     class << self
       attr_accessor :exit_code
     end
+    def exit_code
+      self.class.exit_code
+    end
   end
   
   class VagabondError
@@ -11,7 +14,7 @@ module Vagabond
         invalid_action invalid_template kitchen_missing_yml
         kitchen_no_cookbook_args kitchen_too_many_args
         kitchen_invalid_platform missing_node_name cluster_invalid
-        kitchen_test_failed host_provision_failed
+        kitchen_test_failed host_provision_failed spec_failed
     ).each_with_index do |klass_name, i|
       klass = klass_name.split('_').map(&:capitalize).join
       self.class_eval("class #{klass} < VagabondError; self.exit_code = #{i + 1}; end")
