3a84c3e975e0092a654502931cba65eccf4c9507
Chris Roberts <chrisroberts.code@gmail.com>
2013-07-04 07:26:36 -0700

    Remove vendored cookbooks

--- lib/vagabond/cookbooks/vagabond/metadata.rb
+++
@@ -1,8 +0,0 @@
-name 'vagabond'
-version '1.0.0'
-license 'Apache 2.0'
-maintainer 'Chris Roberts'
-maintainer_email 'chrisroberts.code@gmail.com'
-
-depends 'lxc'
-depends 'apt'
--- lib/vagabond/cookbooks/vagabond/recipes/zero.rb
+++
@@ -1,9 +0,0 @@
-execute 'apt-get update'
-package 'build-essential'
-gem_package 'chef-zero'
-
-execute 'start chef-zero' do
-  command "start-stop-daemon --background --start --quiet --exec #{File.join(node[:languages][:ruby][:bin_dir],'chef-zero')} -- -H #{node[:ipaddress]} -p 80 start"
-  not_if 'netstat -lpt | grep "tcp[[:space:]]" | grep ruby'
-end
-
