dbd6984496ec52a3429ff642d5f976c4e30ddc4f
Chris Roberts <chrisroberts.code@gmail.com>
2013-08-04 19:23:26 -0700

    Use command helpers

--- lib/vagabond/uploader/knife.rb
+++ lib/vagabond/uploader/knife.rb
@@ -7,18 +7,11 @@ module Vagabond
 
       def upload(*args)
         prepare unless args.include?(:no_prepare)
-        com = "knife cookbook upload#{options[:knife_opts]} --all"
+        com = "cookbook upload#{options[:knife_opts]} --all"
         if(options[:cookbook_paths])
           com << " --cookbook-path #{Array(options[:cookbook_paths]).join(':')}"
         end
-        if(File.exists?(knife_config = File.join(store, '.chef/knife.rb')))
-          com << " --config #{knife_config}"
-        end
-        debug(com)
-        cmd = Mixlib::ShellOut.new(com,
-          :live_stream => options[:debug],
-          :cwd => store
-        )
+        cmd = knife_command(com, :cwd => store)
         cmd.run_command
         cmd.error!
       end
