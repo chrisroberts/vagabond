1802c1836aa2efb0a1e120d23dad62dbc6dff5c7
Chris Roberts <chrisroberts.code@gmail.com>
2013-08-04 19:24:52 -0700

    Update command usage to use helpers

--- lib/vagabond/server.rb
+++ lib/vagabond/server.rb
@@ -53,9 +53,7 @@ module Vagabond
         if(File.directory?(File.join(base_dir, 'roles')))
           %w(rb json js).each do |ext|
             next if Dir.glob(File.join(base_dir, "roles", "*.#{ext}")).size == 0
-            cmd = build_command(
-              "knife role from file #{File.join(base_dir, "roles/*.#{ext}")} #{options[:knife_opts]}"
-            )
+            cmd = knife_command("role from file #{File.join(base_dir, "roles/*.#{ext}")}")
             cmd.run_command
             cmd.error!
           end
@@ -70,10 +68,10 @@ module Vagabond
           Dir.glob(File.join(base_dir, "data_bags/*")).each do |b|
             next if %w(. ..).include?(b) || !File.directory?(b)
             coms = [
-              "knife data bag create #{File.basename(b)} #{options[:knife_opts]}",
-              "knife data bag from file #{File.basename(b)} #{options[:knife_opts]} --all"
+              "data bag create #{File.basename(b)}",
+              "data bag from file #{File.basename(b)} --all"
             ].each do |com|
-              cmd = build_command(com)
+              cmd = knife_command(com)
               cmd.run_command
               cmd.error!
             end
@@ -88,9 +86,7 @@ module Vagabond
         if(File.directory?(File.join(base_dir, 'environments')))
           %w(rb json js).each do |ext|
             next if Dir.glob(File.join(base_dir, "environments", "*.#{ext}")).size == 0
-            cmd = build_command(
-              "knife environment from file #{File.join(base_dir, "environments/*.#{ext}")} #{options[:knife_opts]}"
-            )
+            cmd = knife_command("environment from file #{File.join(base_dir, "environments/*.#{ext}")}")
             cmd.run_command
             cmd.error!
           end
@@ -156,10 +152,10 @@ module Vagabond
       # TODO: Pull custom IP option if provided
       config[:daemon] = true
       config[:original] = server_base
+
       ephemeral = Lxc::Ephemeral.new(config)
       e_name = ephemeral.name
-      @internal_config[mappings_key][name] = e_name
-      @internal_config.save
+      internal_config[mappings_key][name] = e_name
       ephemeral.start!(:fork)
       @lxc = Lxc.new(e_name)
     end
@@ -168,16 +164,17 @@ module Vagabond
       if(vagabondfile[:local_chef_server][:zero] || options[:force_zero])
         ui.info ui.color('  -> Bootstrapping chef-zero...', :cyan)
         tem_file = File.expand_path(File.join(File.dirname(__FILE__), 'bootstraps/server-zero.erb'))
-        options[:knife_opts] = " --server-url http://#{lxc.container_ip(20, true)}"
+        knife_config :server_url => "http://#{lxc.container_ip(20, true)}"
       else
         ui.info ui.color('  -> Bootstrapping erchef...', :cyan)
         tem_file = File.expand_path(File.join(File.dirname(__FILE__), 'bootstraps/server.erb'))
-        options[:knife_opts] = " --server-url https://#{lxc.container_ip(20, true)}"
+        knife_config :server_url => "http://#{lxc.container_ip(20, true)}"
       end
       # Scrub before bootstrap
       direct_container_command('rm -rf /var/chef-host/cookbooks')
-      cmd = build_command(
-        "knife bootstrap #{lxc.container_ip(10, true)} --sync-directory " <<
+      # And bootstrap
+      cmd = knife_command(
+        "bootstrap #{lxc.container_ip(10, true)} --sync-directory " <<
         "\"#{internal_config.cookbook_path}:/var/chef-host/cookbooks\" --template-file " <<
         "#{tem_file} -i /opt/hw-lxc-config/id_rsa"
       )
@@ -193,7 +190,7 @@ module Vagabond
         berks_path = vagabondfile[:local_chef_server][:berkshelf][:path]
       end
       berk_uploader = Uploader::Berkshelf.new(
-        vagabondfile.store_directory, options.merge(
+        vagabondfile.build_private_store, options.merge(
           :ui => ui,
           :berksfile => File.join(vagabondfile.directory, berks_path || 'Berksfile'),
           :chef_server_url => options[:knife_opts].to_s.split(' ').last,
@@ -206,7 +203,7 @@ module Vagabond
     def librarian_upload
       ui.info 'Cookbooks being uploaded with librarian'
       librarian_uploader = Uploader::Librarian.new(
-        vagabondfile.generate_store_path, options.merge(
+        vagabondfile.build_private_store, options.merge(
           :ui => ui,
           :cheffile => File.join(vagabondfile.directory, 'Cheffile')
         )
