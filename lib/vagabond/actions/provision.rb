7161109f9f0cea73d2689eba268564c27166dd5d
Chris Roberts <chrisroberts.code@gmail.com>
2013-08-04 19:22:20 -0700

    Use knife helper

--- lib/vagabond/actions/provision.rb
+++ lib/vagabond/actions/provision.rb
@@ -19,22 +19,17 @@ module Vagabond
 
       def do_provision
         ui.info "#{ui.color('Vagabond:', :bold)} Provisioning node: #{ui.color(name, :magenta)}"
-        com = ["#{sudo} knife bootstrap #{lxc.container_ip(10, true)} -d chef-full -N #{name} -i /opt/hw-lxc-config/id_rsa"]
+        com = ["bootstrap #{lxc.container_ip(10, true)} -d chef-full -N #{name} -i /opt/hw-lxc-config/id_rsa"]
         com << "--no-host-key-verify --run-list \"#{config[:run_list].join(',')}\""
         if(config[:environment])
           com << "-E #{config[:environment]}"
         end
-        if(options[:knife_opts])
-          com << options[:knife_opts]
-        end
         if(attributes)
           com << "-j '#{attributes}'"
         end
-        com = com.join(' ')
-        debug(com)
+        cmd = knife_command(com.join(' '), :live_stream => STDOUT, :timeout => 2000)
         # Send the live stream out since people will generally want to
         # know what's happening
-        cmd = Mixlib::ShellOut.new(com, :live_stream => STDOUT, :timeout => 2000)
         cmd.run_command
         # NOTE: cmd.status.success? won't be valid, so check for FATAL
         # TODO: This isn't really the best check, but should be good
