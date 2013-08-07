7187d70680d30bc3ffba07804a57e2fa4539d9e0
Chris Roberts <chrisroberts.code@gmail.com>
2013-08-07 11:47:32 -0700

    Allow using `ssh` to pass commands directly to nodes

--- lib/vagabond/actions/ssh.rb
+++ lib/vagabond/actions/ssh.rb
@@ -7,7 +7,14 @@ module Vagabond
         if(lxc.exists?)
           if(lxc.running?)
             ui.info "#{ui.color('Vagabond:', :bold)} SSH connect to: #{ui.color(name, :cyan)}"
-            exec("#{options[:sudo]}ssh root@#{lxc.container_ip(10, true)} -i /opt/hw-lxc-config/id_rsa -oStrictHostKeyChecking=no")
+            command = ["#{options[:sudo]}ssh root@#{lxc.container_ip(10, true)} -i /opt/hw-lxc-config/id_rsa -oStrictHostKeyChecking=no"]
+            if(@leftover_args)
+              command << "\"#{@leftover_args.join(' ')}\""
+            end
+            File.open('./a.out', 'w') do |file|
+              file.puts "COM: #{command.join(' ')}"
+            end
+            exec command.join(' ')
           else
             ui.error "Node not running: #{name}"
           end
