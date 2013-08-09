2f8f492f348e2bce9e4e054b9693da0ba59ddd28
Chris Roberts <chrisroberts.code@gmail.com>
2013-08-08 18:33:20 -0700

    Use the bundle path to get to the bin

--- lib/vagabond/helpers/commands.rb
+++ lib/vagabond/helpers/commands.rb
@@ -26,7 +26,7 @@ module Vagabond
 
       def via_bundle
         if(defined?(Bundler) && Bundler.bundle_path)
-          "#{File.join(RbConfig::CONFIG['bindir'], 'bundle')} exec "
+          "#{File.join(Bundler.bundle_path, 'bin', 'bundle')} exec "
         end
       end
 
