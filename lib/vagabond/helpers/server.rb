0ee93383097caeacc76749c28c74c3d95161a7ce
Chris Roberts <chrisroberts.code@gmail.com>
2013-08-13 22:49:19 -0700

    Update server helper. Ensure knife config is available

--- lib/vagabond/helpers/server.rb
+++ lib/vagabond/helpers/server.rb
@@ -2,7 +2,7 @@ module Vagabond
   module Helpers
     module Server
       def setup_server_if_needed
-        requrie 'vagabond/server'
+        require 'vagabond/server'
         srv = ::Vagabond::Server.new
         srv.options = options.dup
         unless(vagabondfile.local_chef_server?)
@@ -10,17 +10,19 @@ module Vagabond
         end
         srv.options[:auto_provision] = true
         unless(srv.lxc.running?)
-          srv.send(:setup, 'up')
-          srv.execute
+          vagabondfile.generate_store_path
+          internal_config.make_knife_config_if_required(:force)
+          srv.up
+          knife_config :server_url => "http#{'s' unless srv.lxc.name.include?('zero')}://#{srv.lxc.container_ip(20, true)}"
           srv.send(:upload_cookbooks)
           @srv = srv
         end
+        knife_config :server_url => "http#{'s' unless srv.lxc.name.include?('zero')}://#{srv.lxc.container_ip(20, true)}"
       end
 
       def destroy_server_if_needed
         if(@srv)
-          srv.send(:setup, 'destroy')
-          srv.send(:execute)
+          @srv.destroy
         end
       end
     end
