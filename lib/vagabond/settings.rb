53d1dba9b5125331e80c7dd2c71b0fb1a418e4b6
Chris Roberts <chrisroberts.code@gmail.com>
2013-08-13 19:27:03 -0700

    Add settings to container ephemeral configurations

---
+++ lib/vagabond/settings.rb
@@ -0,0 +1,17 @@
+require 'chef/mash'
+
+module Vagabond
+  class Settings
+    class << self
+      def [](k)
+        @v ||= Mash.new
+        @v[k]
+      end
+      def []=(k,v)
+        @v ||= Mash.new
+        @v[k] = v
+        v
+      end
+    end
+  end
+end
