17cd4c2a50aa55bdddefc6719d9ab400c5ff27c3
Chris Roberts <chrisroberts.code@gmail.com>
2013-09-23 15:34:54 -0700

    Ensure user read only on key for connection

--- lib/vagabond/actions/ssh.rb
+++ lib/vagabond/actions/ssh.rb
@@ -2,12 +2,16 @@
 module Vagabond
   module Actions
     module SSH
+
+      SSH_KEY_BASE = '/opt/hw-lxc-config/id_rsa'
+
       def _ssh
         name_required!
         if(lxc.exists?)
           if(lxc.running?)
+            key_path = setup_key!
             ui.info "#{ui.color('Vagabond:', :bold)} SSH connect to: #{ui.color(name, :cyan)}"
-            command = ["#{options[:sudo]}ssh root@#{lxc.container_ip(10, true)} -i /opt/hw-lxc-config/id_rsa -oStrictHostKeyChecking=no"]
+            command = ["#{options[:sudo]}ssh root@#{lxc.container_ip(10, true)} -i #{key_path} -oStrictHostKeyChecking=no"]
             if(@leftover_args)
               command << "\"#{@leftover_args.join(' ')}\""
             end
@@ -18,6 +22,15 @@ module Vagabond
         else
           ui.error "Node not created: #{name}"
         end
+      end
+
+      def setup_key!
+        path = "/tmp/.#{ENV['USER']}_id_rsa"
+        unless(File.exists?(path))
+          FileUtils.cp(SSH_KEY_BASE, path)
+          FileUtils.chmod(0600, path)
+        end
+        path
       end
     end
   end
