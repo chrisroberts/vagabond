083faccc20efa533d858891d891d23fba755c9ea
Chris Roberts <chrisroberts.code@gmail.com>
2015-01-30 14:59:58 -0800

    On initial seed, serialize empty registry instance into file

--- lib/vagabond/utils/configuration.rb
+++ lib/vagabond/utils/configuration.rb
@@ -9,7 +9,7 @@ module Vagabond
         memoize(:registry) do
           registry_path = File.join(vagabondfile.global_cache, 'registry.json')
           unless(File.exists?(registry_path))
-            File.open(registry_path, 'w'){|f| f.puts '{}'}
+            File.open(registry_path, 'w'){|f| f.puts MultiJson.dump(Registry.new({}))}
           end
           Registry.new(registry_path)
         end
@@ -19,7 +19,7 @@ module Vagabond
       def local_registry
         memoize(:local_registry) do
           unless(registry.entries.get(vagabondfile.fid))
-            registry.entries.set(vagabondfile.fid, Smash.new)
+            registry.entries.set(vagabondfile.fid, Registry::Entry.new)
           end
           registry.entries[vagabondfile.fid]
         end
