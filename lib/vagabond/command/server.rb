2bda42aa9992e76dc3c6492eac3430a550982281
Chris Roberts <chrisroberts.code@gmail.com>
2015-02-08 13:18:58 -0800

    Let server just ride on top of regular commands

--- lib/vagabond/command/server.rb
+++ lib/vagabond/command/server.rb
@@ -4,9 +4,6 @@ module Vagabond
   class Command
     module Server
 
-      autoload :Create, 'vagabond/command/server/create'
-      autoload :Destroy, 'vagabond/command/server/destroy'
-      autoload :Status, 'vagabond/command/server/status'
       autoload :Upload, 'vagabond/command/server/upload'
 
     end
