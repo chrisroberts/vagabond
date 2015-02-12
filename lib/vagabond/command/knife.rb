765b28b3cfe447905bb78256179f87f70f8ff4bd
Chris Roberts <chrisroberts.code@gmail.com>
2015-02-12 06:02:56 -0800

    Add knife command for simply proxy to internal server

---
+++ lib/vagabond/command/knife.rb
@@ -0,0 +1,21 @@
+#encoding: utf-8
+
+require 'vagabond'
+
+module Vagabond
+  class Command
+    # Knife a node
+    class Knife < Command
+
+      # Knife that node
+      def run!
+        server_required!
+        ui.info "`knife #{arguments.join(' ')}`:"
+        cmd = ['knife', *arguments]
+        cmd.push('--server-url').push("https://#{server_node.address}")
+        host_command(cmd, :stream => true)
+      end
+
+    end
+  end
+end
