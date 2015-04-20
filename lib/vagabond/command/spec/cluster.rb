cc52a4aca8a06be1c6c8846a7363cc65c490f1a9
Chris Roberts <chrisroberts.code@gmail.com>
2015-04-20 10:50:40 -0700

    Add initial simple spec handling for clusters

---
+++ lib/vagabond/command/spec/cluster.rb
@@ -0,0 +1,26 @@
+require 'vagabond'
+
+module Vagabond
+  class Command
+    class Spec
+      # Run specs on a cluster
+      class Cluster < Spec
+
+        # Create node
+        def run!
+          if(arguments.size == 1)
+            c_name = arguments.first
+            ui.info "Running cluster spec on #{ui.color(c_name, :bold)}"
+            cluster(c_name).each do |name|
+              ui.info "Running cluster spec on node #{ui.color(name, :bold)}"
+              apply(node(name))
+              ui.info "Cluster spec on node #{ui.color(name, :bold)} is #{ui.color('complete', :green, :bold)}"
+            end
+            ui.info "Cluster spec on #{ui.color(c_name, :bold)} is #{ui.color('complete', :green, :bold)}"
+          end
+        end
+
+      end
+    end
+  end
+end
